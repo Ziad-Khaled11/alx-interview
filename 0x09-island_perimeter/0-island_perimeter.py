a92daa8495aea8d90d3a90c1788b85b1e3dcac7a
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-12-07 21:05:45 +0200

    Mango ;)

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,15 +8,26 @@ def island_perimeter (grid):
     """
     perimeter = 0
     for i in range(len(grid)):
-        for j in range(grid[0]):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 perimeter += 4
-            if i > 0 and grid[i - 1][j] == 1:
-                perimeter -= 1
-            if i < len(grid) -1 and grid[i+1][j] == 1:
-                perimeter -= 1
-            if j > 0 and grid[i][j-1] == 1:
-                perimeter -= 1
-            if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
-                perimeter -= 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < len(grid) -1 and grid[i+1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
     return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
